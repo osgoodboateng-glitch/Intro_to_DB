b9c6f61aaeed9de40ad5cec9ae4b220b3c179fe9
osgoodboateng-glitch <osgoodboateng2006@gmail.com>
2026-02-04 00:51:55 +0000

    Add task4

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Select the correct database
+USE alx_book_store;
+
+-- Print the full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
